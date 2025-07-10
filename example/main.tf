3a0d60c5d82752323c78b4681650340e74abbd8e
Sahil Bansal <sahil.bansal@mycloudwish.com>
2025-07-10 17:05:12 +0530

    added initial commit for network firewall

--- example/main.tf
+++ example/main.tf
@@ -1,5 +1,5 @@
 module "nw" {
   source                = "../"
-  external_vpc_ip_cidr  = ["10.0.0.0/16"]
-  share_tgw_account_ids = ["1234567890"]
+  external_vpc_ip_cidr  = ["20.20.0.0/16"]
+  share_tgw_account_ids = ["752711392763"]
 }
