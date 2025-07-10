b868243bb88b5b6eeca586308637a6dfc2648705
Sahil Bansal <sahil.bansal@mycloudwish.com>
2025-07-10 17:21:02 +0530

    added initial commit for network firewall

--- routes.tf
+++ routes.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.81.0"
-    }
-  }
-}
-
 resource "aws_route" "aws_route" {
   depends_on             = [aws_route_table.aws_route_table_public, aws_ec2_transit_gateway.aws_ec2_transit_gateway]
   for_each               = toset(var.aws_vpc_cidr)
