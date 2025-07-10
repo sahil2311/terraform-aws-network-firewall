a9df953cce79890d6a6d98962020891ae5c30893
Sahil Bansal <sahil.bansal@mycloudwish.com>
2025-07-10 17:40:46 +0530

    added initial commit for network firewall

--- route-associations.tf
+++ route-associations.tf
@@ -11,6 +11,7 @@ resource "aws_route_table_association" "aws_route_table_association_inspection" 
 }
 
 resource "aws_route_table_association" "aws_route_table_association_tgw" {
+  depends_on     = [aws_networkfirewall_firewall.aws_networkfirewall_firewall]
   count          = length(var.aws_cidrs_tgw)
   subnet_id      = element(aws_subnet.aws_subnet_tgw[*].id, count.index)
   route_table_id = element(aws_route_table.aws_route_table_tgw[*].id, count.index)
