a3611be9d30567f7feada4bb5a164d509e2fe674
Sahil Bansal <sahil.bansal@mycloudwish.com>
2025-07-10 17:21:30 +0530

    added initial commit for network firewall

--- routes.tf
+++ routes.tf
@@ -1,6 +1,6 @@
 resource "aws_route" "aws_route" {
   depends_on             = [aws_route_table.aws_route_table_public, aws_ec2_transit_gateway.aws_ec2_transit_gateway]
-  for_each               = toset(var.aws_vpc_cidr)
+  for_each               = toset(var.external_vpc_ip_cidr)
   route_table_id         = aws_route_table.aws_route_table_public.id
   destination_cidr_block = each.value
   transit_gateway_id     = aws_ec2_transit_gateway.aws_ec2_transit_gateway.id
