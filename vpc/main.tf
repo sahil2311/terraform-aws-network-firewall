6c646545fb17de3a942110e08345aa2b3109edda
Sahil Bansal <sahil.bansal@mycloudwish.com>
2025-07-10 19:05:51 +0530

    added initial commit for network firewall

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,7 +1,8 @@
 module "nw" {
-  source                = "../"
-  external_vpc_ip_cidr  = ["172.31.0.0/16"]
-  share_tgw_account_ids = ["752711392763"]
-  aws_region            = var.aws_region
-  environment           = var.environment
+  source                          = "../"
+  external_vpc_ip_cidr            = ["172.31.0.0/16"]
+  share_tgw_account_ids           = ["752711392763"]
+  attached_stateful_managed_rules = true
+  aws_region                      = var.aws_region
+  environment                     = var.environment
 }
